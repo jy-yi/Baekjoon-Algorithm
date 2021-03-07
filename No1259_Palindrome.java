49856755d984405421eb853fc805a2aa10f33e20
종윤 <jongyoon9515@gmail.com>
2021-03-07 18:39:22 +0900

    [백준 알고리즘] 1259번 / 팰린드롬수

---
+++ No1259_Palindrome.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * 팰린드롬수
+ * @author jy-yi
+ *
+ * 어떤 단어를 뒤에서부터 읽어도 똑같다면 그 단어를 팰린드롬이라고 한다. 'radar', 'sees'는 팰린드롬이다.
+ * 
+ * 수도 팰린드롬으로 취급할 수 있다. 
+ * 수의 숫자들을 뒤에서부터 읽어도 같다면 그 수는 팰린드롬수다. 
+ * 121, 12421 등은 팰린드롬수다. 
+ * 123, 1231은 뒤에서부터 읽으면 다르므로 팰린드롬수가 아니다. 
+ * 
+ * 또한 10도 팰린드롬수가 아닌데, 앞에 무의미한 0이 올 수 있다면 010이 되어 팰린드롬수로 취급할 수도 있지만, 
+ * 특별히 이번 문제에서는 무의미한 0이 앞에 올 수 없다고 하자.
+ */
+public class No1259_Palindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			int n = sc.nextInt();
+			
+			if (n == 0) {
+				sc.close();
+				return;
+			}
+			
+			int original = n;
+			int reverse = 0;
+			
+			while(n > 0) {
+				reverse = reverse * 10 + n % 10;
+				n /= 10;
+			}
+			
+			if (original == reverse) System.out.println("yes");
+			else System.out.println("no");
+			
+		}
+		
+	}
+}
